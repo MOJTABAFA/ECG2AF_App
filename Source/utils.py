c75e98a295681edcba053b022d83fca7efd49bce
MOJTABA FAZLI <MOJTABAFA@users.noreply.github.com>
2024-11-04 08:26:35 -0800

    Update utils.py

--- Source/utils.py
+++ Source/utils.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import numpy as np
 
 # Path to the pre-trained model
-MODEL_PATH = 'model_zoo/ECG2AF/ecg_5000_survival_curve_af_quadruple_task_mgh_v2021_05_21.h5'
+MODEL_PATH = 'ml4h/model_zoo/ECG2AF/ecg_5000_survival_curve_af_quadruple_task_mgh_v2021_05_21.h5'
 
 def load_model():
     """
@@ -11,6 +12,16 @@ def load_model():
     """
     model = tf.keras.models.load_model(MODEL_PATH)
     return model
+
+def ecg_as_tensor(ecg_data):
+    """
+    Convert ECG data to a tensor format suitable for the model.
+    Args:
+        ecg_data (list or np.ndarray): Raw ECG data.
+    Returns:
+        tf.Tensor: The ECG data as a tensor.
+    """
+    return tf.convert_to_tensor(ecg_data, dtype=tf.float32)
 
 def predict_ecg(model, tensor):
     """
